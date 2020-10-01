9c8ec463b64c5b8eb259ccf53524685688e82226
shekharAggarwal <aggarwalshekhar99@gmail.com>
2020-10-01 17:00:50 +0530

    login with email

--- lib/amplifyconfiguration.dart
+++ lib/amplifyconfiguration.dart
@@ -1,4 +1,36 @@
 const amplifyconfig = ''' {
     "UserAgent": "aws-amplify-cli/2.0",
-    "Version": "1.0"
+    "Version": "1.0",
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "ap-south-1:07fcba74-60f2-4290-828e-45c7ff5499cc",
+                            "Region": "ap-south-1"
+                        }
+                    }
+                },
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "ap-south-1_beRz2L9s1",
+                        "AppClientId": "52jrmuffo7jls43gs7j32s4m1s",
+                        "AppClientSecret": "1am3h7egp8senfblb6ssc64le9l5d4uvd412f9jr7fp7v3l1gj12",
+                        "Region": "ap-south-1"
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH"
+                    }
+                }
+            }
+        }
+    }
 }''';
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_core/amplify_core.dart';
 import 'package:aws_project/amplifyconfiguration.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_login/flutter_login.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 void main() {
   runApp(MaterialApp(home: MyApp()));
@@ -13,6 +16,8 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   bool _amplifyConfigured = false;
+  bool isSignUpComplete = false;
+  bool isSignInComplete = false;
 
   Amplify amplifyInstance = Amplify();
   @override
@@ -24,19 +29,65 @@ class _MyAppState extends State<MyApp> {
   void _configureAmplify() async {
     if (!mounted) return;
     try {
-    await amplifyInstance.configure(amplifyconfig);
+      AmplifyAuthCognito authPlugin = AmplifyAuthCognito();
+      amplifyInstance.addPlugin(authPlugins: [authPlugin]);
+      await amplifyInstance.configure(amplifyconfig);
       setState(() {
         _amplifyConfigured = true;
+        print(_amplifyConfigured ? "configured" : "not yet");
       });
     } catch (e) {
       print(e);
     }
   }
 
+  Future<String> _registerUser(LoginData data) async {
+    try {
+      SignUpResult res = await Amplify.Auth.signUp(
+          username: data.name, password: data.password);
+      setState(() {
+        isSignUpComplete = res.isSignUpComplete;
+        print("sing up " + (isSignUpComplete ? "completed" : "not completed"));
+      });
+      return "";
+    } on AuthError catch (e) {
+      return e.toString();
+    }
+  }
+
+  Future<String> _loginUser(LoginData data) async {
+    try {
+      SignInResult res = await Amplify.Auth.signIn(
+          username: data.name, password: data.password);
+      setState(() {
+        isSignInComplete = res.isSignedIn;
+        Alert(
+                context: context,
+                type: AlertType.success,
+                title: "Login success",
+                desc: "Good Job")
+            .show();
+      });
+      return "";
+    } on AuthError catch (e) {
+      Alert(
+              context: context,
+              type: AlertType.error,
+              title: "Login Failed",
+              desc: e.toString())
+          .show();
+      return e.toString();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(_amplifyConfigured ? "Configured" : "not configured"),
-    );
+    return SafeArea(
+        child: FlutterLogin(
+      onSignup: _registerUser,
+      onLogin: _loginUser,
+      onRecoverPassword: (_) => null,
+      title: "AWS Amplify",
+    ));
   }
 }
